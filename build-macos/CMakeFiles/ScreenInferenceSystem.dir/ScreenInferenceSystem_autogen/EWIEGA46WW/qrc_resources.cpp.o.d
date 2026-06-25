87a3a8f95a8ff893e99b1cdc276c956cfea5d9c7
mundusG <1245714534@qq.com>
2026-06-25 10:43:58 +0800

    停止追踪build

--- build-macos/CMakeFiles/ScreenInferenceSystem.dir/ScreenInferenceSystem_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/ScreenInferenceSystem.dir/ScreenInferenceSystem_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
-  /Users/young/Desktop/Kinth/Screen_system/build-macos/ScreenInferenceSystem_autogen/EWIEGA46WW/qrc_resources.cpp
